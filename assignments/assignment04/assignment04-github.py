ae65ad1a7cac496fc54dfd7804717d8d58c8d18d
Mr-Scarf <dscal07@outlook.com>
2026-03-14 14:23:54 +0000

    Lab 4 + assignment 4

---
+++ assignments/assignment04/assignment04-github.py
@@ -0,0 +1,14 @@
+# Lab Instruction : https://vlegalwaymayo.atu.ie/mod/page/view.php?id=1393877
+
+#Write a program in python that will read a file from a repository, 
+#The program should then replace all the instances of the text "Andrew" with your name. 
+#The program should then commit those changes and push the file back to the repository (You will need authorisation to do this).
+#I do not need to see your keys (see lab2)
+#Handup: Push the program as assignment04-github.py to assignments repository.
+#Marks: Marks will be given for the functionality and layout of the code; I do expect minimal comments to indicate you know what the program is doing.
+
+
+import requests
+import json
+from config import config as cfg
+
